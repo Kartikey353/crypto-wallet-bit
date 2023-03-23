6920c62348ceff6c577234aaac13c620d17562cb
Varunjaishreeram <varunbharadwaj444@gmail.com>
2023-03-23 12:17:01 +0530

    adding import page

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -54,7 +54,7 @@ const Home = () => {
   }, [account, currentNetwork]);
 
   return (
-    <div className="relative container py-10 ">
+    <div className="relative container py-10">
       <Header />
       <div className="rounded-lg flex bg-dark-600 p-3 justify-between items-center mt-8">
         <p>Account 1</p>
